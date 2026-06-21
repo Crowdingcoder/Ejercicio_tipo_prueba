c6d3c4bd71161f863ffe913868b2f9000dff8ce2
crowdingcoder <crowdingcoder@192.168.1.24>
2026-06-20 22:14:11 -0400

    programilogrado

--- functions.py
+++ functions.py
@@ -5,12 +5,39 @@ import time
 productos =[]
 
 def limpiar():
-    os.system("cls")
+    os.system("clear")
 
 def pausar():
     time.sleep(2)
+#menu
+def mostrar_menu():
+    print("====MENÚ PRINCIPAL====")
+    print("1.Agregar producto")
+    print("2.Buscar producto")
+    print("3.Eliminar producto")
+    print("4.Actualizar disponibilidad")
+    print("5.Mostrar productos")
+    print("6.Salir")
 
-#validaciones
+#opciones
+def leer_opcion():
+    try:
+        opcion= int(input("Ingrese opcion:\n"))
+        if opcion >= 1 and opcion <= 6:
+            return opcion
+        else:
+            print("Opcion ingresada no existe.")
+    except:
+        print("El valor ingresado debe ser numerico")
+
+#validacione
+def validar_lista():
+    if len(productos) <= 0:
+        print("No se han registrado productos.")
+        return False
+    else:
+        return True
+
 def validar_entero(entero):
     if entero >= 0:
         return True
@@ -23,17 +50,92 @@ def validar_str(string):
     else:
         print("El campo no puede venir vacio.")
 
-#agregar cosas
-def agregar_producto(producto):
+def validar_float(decimal):
+    if isinstance(decimal,float) and decimal >= 0:
+        return True
+    else:
+        print("El precio no puede ser menor que 0.")
+#agregar cosas (opcion 1)
+def agregar_producto():
+    print("====Agregar Producto====")
+    #nombre del producto
     while True:
-        nombre= input("Ingrese nombre del producto:\n")
+        nombre= input("Ingrese nombre del producto:\n").strip()
         if validar_str(nombre):
             break
-
-def agregar_stock(stock):
+    #stock del producto
     while True:
-        cantidad = int(input("Ingrese stock del producto:\n"))
-        if validar_entero(cantidad):
-            break
+        try:
+            stock = int(input("Ingrese stock del producto:\n"))
+            if validar_entero(stock):
+                break
+        except:
+            print("El valor ingresado debe ser numerico.")
+    #precio del producto
+    while True:
+        try:
+            precio = float(input("Ingrede precio del producto:\n"))
+            if validar_float(precio):
+                break
+        except:
+            print("El valor ingresado debe ser numerico.")
+    producto = {"nombre" : nombre,
+                "stock" : stock,
+                "precio" : precio,
+                "disponible" : False
+
+    }
+    productos.append(producto)
+    print("Producto agregado de manera exitosa.")
+    pausar()
+#Print estado producto
+def estado(estado):
+    if estado == True:
+        print("Estado producto: DISPONIBLE.")
+    else:
+        print("Estado producto: SIN STOCK.")
+
+#buscar producto (opcion 2)
+def buscar_producto(buscar):
+    for p in range (len(productos)): #de esta forma p tiene un valor numerico y no de todo el diccionario
+        if productos[p]["nombre"] == buscar:
+            return p
+    return -1
+
+def print_buscar(posicion):
+    if posicion == -1:
+        print("El producto buscado no se encuentra registrado")
+    else: 
+        print(f"Nombre producto: {productos[posicion]["nombre"]}")
+        print(f"Stock disponible: {productos[posicion]["stock"]}")
+        print(f"Precio producto: {productos[posicion]["precio"]}")
+        estado(productos[posicion]["disponible"]) #utilizar el true o false de disponible con la funcion estado
+#eliminar producto (opcion 3)
+def eliminar_producto(posicion): 
+    if posicion == -1:
+        return True #para despues utilizar el nombre del producto en main
+    else:
+        productos.remove(productos[posicion])
+        print("El producto ha sido eliminado.")
+    
+
+#actualizar disponibilidad (opcion 4)
+
+def actualizar_disponibilidad():
+    for p in productos:
+        if p["stock"] > 0:
+            p["disponible"] = True
+        else:
+            p["disponible"] = False
 
 
+#lista de productos (opcion 5)
+def mostrar_productos():
+    actualizar_disponibilidad()
+    for p in productos:
+        print(f"Nombre producto: {p["nombre"]}")
+        print(f"Stock disponible: {p["stock"]}")
+        print(f"Precio producto: {p["precio"]}")
+        estado(p["disponible"])
+        print("*************************")
+    pausar()
--- main.py
+++ main.py
@@ -1,7 +1,39 @@
 from functions import *
 limpiar()
+banderita = True
 
-agregar_producto(productos)
-agregar_stock()
-
-
+print("Bienvenido a nuestro programiregistrador de productos.\n")
+while banderita:
+    mostrar_menu()
+    opcion = leer_opcion()
+    if opcion == 1:
+        agregar_producto()
+    elif opcion == 2:
+        print("====Buscar Producto====\n")
+        if validar_lista():
+            buscar = input("Ingrese producto a buscar:\n").strip()
+            posicion = buscar_producto(buscar)
+            print_buscar(posicion)
+    elif opcion == 3:
+        print("====Eliminar Producto====")
+        if validar_lista():
+            eliminar = input("Ingrese producto a eliminar:\n").strip()
+            posicion = buscar_producto(eliminar)
+            eliminar_producto(posicion)
+            if eliminar_producto(posicion): #si es verdadero se realiza esto
+                print(f"El producto {eliminar} no se encuentra registrado.") #asi podemos decir el nmbre del producto a buscar
+            pausar()
+    elif opcion == 4:
+        print("====Actualizar Disponibilidad====")
+        if validar_lista():
+            actualizar_disponibilidad()
+            print("Disponibilidad actualizada.")
+        pausar()
+    elif opcion == 5:
+        print("====Mostrar Productos")
+        if validar_lista():
+            mostrar_productos()
+    elif opcion == 6:
+        banderita = False
+        limpiar()
+        print("Gracias por usar el sistema. Vuelva Pronto.")
